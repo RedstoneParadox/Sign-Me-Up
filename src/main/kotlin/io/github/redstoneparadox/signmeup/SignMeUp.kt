bde4f7df1fda72bd7f8b0edb555515ec7087dbf0
RedstoneParadox <spinjitzu00@gmail.com>
2021-07-19 16:05:18 -0700

    Initial setup

---
+++ src/main/kotlin/io/github/redstoneparadox/signmeup/SignMeUp.kt
@@ -0,0 +1,11 @@
+package io.github.redstoneparadox.signmeup
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("unused")
+object SignMeUp: ModInitializer {
+    override fun onInitialize() {
+        println("Hello Fabric world!")
+    }
+}
+
